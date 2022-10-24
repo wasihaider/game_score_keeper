7cc7ad5a8a7f510ec2f78edf9eaac09d2940bb03
Wasi Haider <wsi.haidr@gmail.com>
2022-10-24 14:46:41 +0500

    Delete player app

--- scorekeeper/game/models.py
+++ scorekeeper/game/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Game(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=250)
+
+
+
+
+
+class Match(models.Model):
+    id = models.AutoField(primary_key=True)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, null=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
